1398b1dc01ed70ab8adf58087af00c505e79e00f
Junx wan <hugh8845sam@gmail.com>
2019-04-30 12:17:54 +0800

    feat: 拿回

--- internal/comet/errors/errors.go
+++ internal/comet/errors/errors.go
@@ -5,6 +5,13 @@ import (
 )
 
 var (
+	// ring
+	ErrRingEmpty = errors.New("ring buffer empty")
+	ErrRingFull  = errors.New("ring buffer full")
+
+	// channel
+	ErrPushMsgArg   = errors.New("rpc pushmsg arg error")
+
 	// bucket
 	ErrBroadCastArg     = errors.New("rpc broadcast arg error")
 	ErrBroadCastRoomArg = errors.New("rpc broadcast  room arg error")
