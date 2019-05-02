ddc4fbb924753420d4cc6c32656117b9a7d0a222
zorn <zorntsai@jetfuel.com.tw>
2019-05-02 18:07:02 +0800

    feat: ApiHttpServer refactoring

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module gitlab.com/jetfueltw/cpw/alakazam
 
 require (
 	github.com/Shopify/sarama v1.22.1 // indirect
+	github.com/Terry-Mao/goim v0.0.0-20190424061838-af040a2647fa
 	github.com/bsm/sarama-cluster v2.1.15+incompatible
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
@@ -11,12 +12,10 @@ require (
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/uuid v1.0.0
 	github.com/mattn/go-isatty v0.0.7 // indirect
-	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.3.0
 	github.com/zhenjl/cityhash v0.0.0-20131128155616-cdd6a94144ab
 	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
 	google.golang.org/grpc v1.16.0
 	gopkg.in/Shopify/sarama.v1 v1.19.0
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 )
--- internal/logic/http/api_v1.go
+++ internal/logic/http/api_v1.go
@@ -1,91 +1,93 @@
 package http
 
 import (
-	"context"
 	stdErrors "errors"
-	"fmt"
-	"io/ioutil"
+	"fmt" /*should remove finally*/
 
 	"github.com/gin-gonic/gin"
 )
 
+type apiCommon interface {
+	mapParamters(c *gin.Context) error
+	payLoadToJson() map[string]interface{}
+	payLoadTransfer()
+	businessLogic()
+}
+
+//RequestCommonPayLoad ...
+//通用 request payload
 type RequestCommonPayLoad struct {
-	action, from, to, query, content, Type, Room string
-	Op, Speed                                    int32
-	Mids                                         []int64
-	msg                                          []byte
+	action    string
+	from      string
+	to        string
+	query     string
+	content   string
+	Type      string
+	Room      string
+	Op, Speed int32
+	Mids      []int64
+	msg       []byte
 }
 
-// 以user key來推送訊息
-func (s *Server) copyPushKeys(c *gin.Context) {
-	var arg struct {
-		Op   int32    `form:"operation"`
-		Keys []string `form:"keys"`
-	}
-	if err := c.BindQuery(&arg); err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	msg, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	if err = s.logic.PushKeys(context.TODO(), arg.Op, arg.Keys, msg); err != nil {
-		result(c, nil, RequestErr)
-		return
-	}
-	result(c, nil, OK)
+//Jinyan ...
+//禁言
+type Jinyan struct {
+	RequestCommonPayLoad
 }
 
-// 以user id來推送訊息
-func (s *Server) copyPushMids(c *gin.Context) {
-	var arg struct {
-		Op   int32   `form:"operation"`
-		Mids []int64 `form:"mids"`
+//MapParamters ...
+//resolveURL implementation
+func (j *Jinyan) mapParamters(c *gin.Context) error {
+
+	if c.Param("from") == "" /* && TODO: search white list */ {
+		return stdErrors.New("房間參數出錯")
 	}
-	if err := c.BindQuery(&arg); err != nil {
-		errors(c, RequestErr, err.Error())
-		return
+	if c.Param("to") == "" /* && TODO: search white list */ {
+		return stdErrors.New("指定用戶出錯")
 	}
-	msg, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	if err = s.logic.PushMids(context.TODO(), arg.Op, arg.Mids, msg); err != nil {
-		errors(c, ServerErr, err.Error())
-		return
-	}
-	result(c, nil, OK)
+
+	j.action = "禁言"
+	j.from = c.Param("from")
+	j.to = c.Param("to")
+	j.content = c.Param("content")
+	j.msg = []byte(c.Param("content"))
+
+	return nil
 }
 
-// 以operation來推送訊息
-func (s *Server) copyPushAll(c *gin.Context) {
-	var arg struct {
-		Op    int32 `form:"operation" binding:"required"`
-		Speed int32 `form:"speed"`
+//PayLoadToJSON ...
+// 將request payload 轉成 json
+func (j *Jinyan) payLoadToJSON() map[string]interface{} {
+	//TODO: 需要實做 check error
+	return gin.H{
+		"action":  j.action,
+		"from":    j.from,
+		"to":      j.to,
+		"content": j.content,
+		"Type":    j.Type,
+		"Room":    j.Room,
+		"Op":      j.Op,
+		"Speed":   j.Speed,
 	}
-	if err := c.BindQuery(&arg); err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	msg, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	if err = s.logic.PushAll(c, arg.Op, arg.Speed, msg); err != nil {
-		errors(c, ServerErr, err.Error())
-		return
-	}
-	result(c, nil, OK)
 }
 
-//禁言  以user id來推送訊息
-//封鎖  以user id來推送訊息
-//紅包 以operation來推送訊息
-//假人 以room id來推送訊息
+// PayLoadTransfer ...
+// 將 payload 轉成goim domain 資料
+func (j *Jinyan) payLoadTransfer() {
+	//TODO: 需要實做 check error
+	j.Type = "Type 123"
+	j.Room = "Room 234"
+	j.Op = 123
+	j.Speed = 123
+	j.Mids = []int64{1, 2, 3, 4}
+}
+
+// BusinessLogic ...
+// 禁言專用 BusinessLogic 處理
+func (j *Jinyan) businessLogic() {
+	//TODO: 需要實做 check error
+	fmt.Println("禁言專用 BusinessLogic 處理 ... ")
+}
 
 func (s *Server) base(c *gin.Context) {
 	//讀取 request uri binding
@@ -102,117 +104,28 @@ func (s *Server) base(c *gin.Context) {
 	result(c, gin.H{"health": "I am fine"}, OK)
 }
 
-/*
-
-  //禁言
-  v1.POST("/jinyan/:room/:uid/:content",s.jinyan)
-  //封鎖
-  v1.POST("/fengsuo/:room/:uid/:content",s.fengsuo)
-  //紅包
-  v1.POST("/hongbao/:room/:uid/:content",s.hongbao)
-  //假人
-  v1.POST("/faker/:room/:uid/:content",s.faker)
-
-
-*/
-
-// 以room id來推送訊息
-func (s *Server) copyPushRoom(c *gin.Context) {
-	var arg struct {
-		Op   int32  `form:"operation" binding:"required"`
-		Type string `form:"type" binding:"required"`
-		Room string `form:"room" binding:"required"`
-	}
-	if err := c.BindQuery(&arg); err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	msg, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		errors(c, RequestErr, err.Error())
-		return
-	}
-	if err = s.logic.PushRoom(c, arg.Op, arg.Type, arg.Room, msg); err != nil {
-		errors(c, ServerErr, err.Error())
-		return
-	}
-	result(c, nil, OK)
-}
-
-func fromURLMap(action string, c *gin.Context, payload *RequestCommonPayLoad) error {
-
-	if action == "" /* && TODO: search white list */ {
-		return stdErrors.New("action參數出錯")
-	}
-	if c.Param("from") == "" /* && TODO: search white list */ {
-		return stdErrors.New("房間參數出錯")
-	}
-	if c.Param("to") == "" /* && TODO: search white list */ {
-		return stdErrors.New("指定用戶出錯")
-	}
-
-	payload.action = action
-	payload.from = c.Param("from")
-	payload.to = c.Param("to")
-	payload.content = c.Param("content")
-	payload.msg = []byte(c.Param("content"))
-
-	return nil
-}
-
-func payloadTransfer(load *RequestCommonPayLoad) {
-	load.Type = "Type 123"
-	load.Room = "Room 234"
-	load.Op = 123
-	load.Speed = 123
-	load.Mids = []int64{1, 2, 3, 4}
-}
-
-func businessLogic(load *RequestCommonPayLoad) {
-	fmt.Println("[business logic process]")
-}
-
-func dumpRequestPayLoad(p RequestCommonPayLoad) map[string]interface{} {
-	return gin.H{
-		"action":  p.action,
-		"from":    p.from,
-		"to":      p.to,
-		"content": p.content,
-		"Type":    p.Type,
-		"Room":    p.Room,
-		"Op":      p.Op,
-		"Speed":   p.Speed,
-	}
-}
-
 func (s *Server) jinyan(c *gin.Context) {
 
-	var load RequestCommonPayLoad
-	//讀取 request uri binding
-	//依 API route 規定驗證 uri binding
-	//TODO: check param goes here
-	if err := fromURLMap("禁言", c, &load); err != nil {
+	//商業邏輯物件
+	jinyan := new(Jinyan)
+
+	//從 URI 取值	
+	if err := jinyan.mapParamters(c); err != nil {
 		errors(c, RequestErr, err.Error())
 		return
 	}
 
 	//資料轉換
-	//TODO
-	payloadTransfer(&load)
+	jinyan.payLoadTransfer()
 
 	//處理request相關的商業邏輯
-	//TODO
-	businessLogic(&load)
+	jinyan.businessLogic()
 
 	//將處理訊息丟向 logic
-	/*
-		if err := s.logic.PushRoom(c, load.Op, load.Type, load.Room, load.msg); err != nil {
-			errors(c, ServerErr, err.Error())
-			return
-		}
-	*/
+	//TODO
+
 	//回覆 Response
-	result(c, dumpRequestPayLoad(load), OK)
+	result(c, jinyan.payLoadToJSON(), OK)
 }
 func (s *Server) fengsuo(c *gin.Context) {
 
