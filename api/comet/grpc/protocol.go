e98a47cdcab7e655a95afc58a6c84d41034102e3
Junx wan <hugh8845sam@gmail.com>
2019-05-02 14:27:27 +0800

    refactor: 移除沒用method

--- api/comet/grpc/protocol.go
+++ api/comet/grpc/protocol.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"errors"
 	"gitlab.com/jetfueltw/cpw/alakazam/internal/protocol"
-	"gitlab.com/jetfueltw/cpw/alakazam/pkg/bufio"
 	"gitlab.com/jetfueltw/cpw/alakazam/pkg/bytes"
 	"gitlab.com/jetfueltw/cpw/alakazam/pkg/encoding/binary"
 	"gitlab.com/jetfueltw/cpw/alakazam/pkg/websocket"
@@ -106,82 +105,6 @@ func (p *Proto) WriteTo(b *bytes.Writer) {
 	if p.Body != nil {
 		b.Write(p.Body)
 	}
-}
-
-// 從tcp中讀出封包並解成Proto格式
-func (p *Proto) ReadTCP(rr *bufio.Reader) (err error) {
-	var (
-		bodyLen   int
-		headerLen int16
-		packLen   int32
-		buf       []byte
-	)
-	if buf, err = rr.Pop(_rawHeaderSize); err != nil {
-		return
-	}
-	packLen = binary.BigEndian.Int32(buf[_packOffset:_headerOffset])
-	headerLen = binary.BigEndian.Int16(buf[_headerOffset:_verOffset])
-	p.Ver = int32(binary.BigEndian.Int16(buf[_verOffset:_opOffset]))
-	p.Op = binary.BigEndian.Int32(buf[_opOffset:_seqOffset])
-	p.Seq = binary.BigEndian.Int32(buf[_seqOffset:])
-	if packLen > _maxPackSize {
-		return ErrProtoPackLen
-	}
-	if headerLen != _rawHeaderSize {
-		return ErrProtoHeaderLen
-	}
-	if bodyLen = int(packLen - int32(headerLen)); bodyLen > 0 {
-		p.Body, err = rr.Pop(bodyLen)
-	} else {
-		p.Body = nil
-	}
-	return
-}
-
-// 將Proto格式資料寫入tcp
-func (p *Proto) WriteTCP(wr *bufio.Writer) (err error) {
-	var (
-		buf     []byte
-		packLen int32
-	)
-	if p.Op == protocol.OpRaw {
-		_, err = wr.WriteRaw(p.Body)
-		return
-	}
-	packLen = _rawHeaderSize + int32(len(p.Body))
-	if buf, err = wr.Peek(_rawHeaderSize); err != nil {
-		return
-	}
-	binary.BigEndian.PutInt32(buf[_packOffset:], packLen)
-	binary.BigEndian.PutInt16(buf[_headerOffset:], int16(_rawHeaderSize))
-	binary.BigEndian.PutInt16(buf[_verOffset:], int16(p.Ver))
-	binary.BigEndian.PutInt32(buf[_opOffset:], p.Op)
-	binary.BigEndian.PutInt32(buf[_seqOffset:], p.Seq)
-	if p.Body != nil {
-		_, err = wr.Write(p.Body)
-	}
-	return
-}
-
-// tcp寫入心跳回覆結果
-func (p *Proto) WriteTCPHeart(wr *bufio.Writer, online int32) (err error) {
-	var (
-		buf     []byte
-		packLen int
-	)
-	packLen = _rawHeaderSize + _heartSize
-	if buf, err = wr.Peek(packLen); err != nil {
-		return
-	}
-	// header
-	binary.BigEndian.PutInt32(buf[_packOffset:], int32(packLen))
-	binary.BigEndian.PutInt16(buf[_headerOffset:], int16(_rawHeaderSize))
-	binary.BigEndian.PutInt16(buf[_verOffset:], int16(p.Ver))
-	binary.BigEndian.PutInt32(buf[_opOffset:], p.Op)
-	binary.BigEndian.PutInt32(buf[_seqOffset:], p.Seq)
-	// body
-	binary.BigEndian.PutInt32(buf[_heartOffset:], online)
-	return
 }
 
 // 從websocket讀出內容
