a2ed5b42d73a0716db5abc547579b7c59b1613d3
Junx wan <hugh8845sam@gmail.com>
2019-05-16 16:21:05 +0800

    feat: add test

--- server/logic/logic_test.go
+++ server/logic/logic_test.go
@@ -1,6 +1,7 @@
 package logic
 
 import (
+	"github.com/stretchr/testify/assert"
 	"gitlab.com/jetfueltw/cpw/alakazam/server/logic/conf"
 	"gitlab.com/jetfueltw/cpw/alakazam/server/logic/dao"
 	"os"
@@ -27,4 +28,9 @@ func initTestConfig() {
 	if err := conf.Read("../../test/run/logic.yml"); err != nil {
 		panic(err)
 	}
+}
+
+func addUser(t *testing.T, uid, key, roomId, name string) {
+	err := d.AddMapping(uid, key, roomId, name, "", 0)
+	assert.Nil(t, err)
 }
---
+++ server/logic/push_test.go
@@ -0,0 +1,53 @@
+package logic
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/jetfueltw/cpw/alakazam/server/errors"
+	"testing"
+)
+
+func TestPushRoomNotLogin(t *testing.T) {
+	p := &PushRoomForm{
+		Uid:     "",
+		Key:     "",
+		Message: "",
+	}
+	err := l.PushRoom(p)
+	assert.Equal(t, errors.LoginError, err)
+}
+
+func TestPushRoomNotRoomIdEmpty(t *testing.T) {
+	addUser(t, "123", "", "", "test")
+
+	p := &PushRoomForm{
+		Uid:     "123",
+		Key:     "",
+		Message: "",
+	}
+	err := l.PushRoom(p)
+	assert.Equal(t, errors.RoomError, err)
+}
+
+func TestPushRoomNotKey(t *testing.T) {
+	addUser(t, "123", "1", "1000", "test")
+
+	p := &PushRoomForm{
+		Uid:     "123",
+		Key:     "",
+		Message: "",
+	}
+	err := l.PushRoom(p)
+	assert.Equal(t, errors.RoomError, err)
+}
+
+func TestPushRoomNotUid(t *testing.T) {
+	addUser(t, "123", "1", "1000", "test")
+
+	p := &PushRoomForm{
+		Uid:     "",
+		Key:     "1",
+		Message: "",
+	}
+	err := l.PushRoom(p)
+	assert.Equal(t, errors.LoginError, err)
+}
