30697f908c0e1983679a3baa30afd24b537eac7b
Junx wan <hugh8845sam@gmail.com>
2019-05-16 11:51:54 +0800

    refactor: 整理

--- server/logic/banned.go
+++ server/logic/banned.go
@@ -10,18 +10,20 @@ func (l *Logic) SetBanned(uid, remark string, expired int) error {
 }
 
 func (l *Logic) IsBanned(uid string, status int) bool {
-	if business.IsBanned(status) {
-		_, ok, err := l.dao.GetBanned(uid)
-		if err != nil {
-			log.Errorf("dao.GetBanned(uid: %s) error(%v)", uid, err)
-		}
-		if ok {
-			return true
-		}
-		if err := l.dao.DelBanned(uid); err != nil {
-			log.Errorf("dao.DelBanned(uid: %s) error(%v)", uid, err)
-			return true
-		}
+	if !business.IsBanned(status) {
+		return false
+	}
+	_, ok, err := l.dao.GetBanned(uid)
+	if err != nil {
+		log.Errorf("dao.GetBanned(uid: %s) error(%v)", uid, err)
+		return false
+	}
+	if ok {
+		return true
+	}
+	if err := l.dao.DelBanned(uid); err != nil {
+		log.Errorf("dao.DelBanned(uid: %s) error(%v)", uid, err)
+		return true
 	}
 	return false
 }
