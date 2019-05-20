dda0afbc3ca28e61dbc39a7caa96a0abf0695a43
Junx wan <hugh8845sam@gmail.com>
2019-05-20 14:11:15 +0800

    feat: database unit test

---
+++ logic/store/store_test.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/magiconair/properties/assert"
+	"gitlab.com/jetfueltw/cpw/alakazam/logic/conf"
+	"os"
+	"testing"
+)
+
+var (
+	store *Store
+	mock  sqlmock.Sqlmock
+)
+
+func TestMain(m *testing.M) {
+	d, mo, err := sqlmock.New()
+	if err != nil {
+		panic(err)
+	}
+	store = &Store{d}
+	mock = mo
+	os.Exit(m.Run())
+}
+
+func TestDatabaseDns(t *testing.T) {
+	c := &conf.Database{
+		Host:      "127.0.0.1",
+		Port:      ":3306",
+		Database:  "database",
+		User:      "root",
+		Password:  "root",
+		Charset:   "utf8mb4",
+		Collation: "utf8mb4_unicode_ci",
+	}
+	dns := DatabaseDns(c)
+
+	assert.Equal(t, "root:root@tcp(127.0.0.1::3306)/database?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true&timeout=2s&loc=Local", dns)
+}
