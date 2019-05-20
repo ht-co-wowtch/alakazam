81d8cdda8067092bcfc56efe319772109cddf240
Junx wan <hugh8845sam@gmail.com>
2019-05-20 17:15:25 +0800

    feat: test

--- test/admin/admin_test.go
+++ test/admin/admin_test.go
@@ -1,13 +1,35 @@
 package admin
 
 import (
+	"gitlab.com/jetfueltw/cpw/alakazam/logic/cache"
+	"gitlab.com/jetfueltw/cpw/alakazam/logic/conf"
 	"gitlab.com/jetfueltw/cpw/alakazam/test/internal/run"
 	"os"
 	"testing"
 )
 
+var (
+	d *cache.Cache
+)
+
 func TestMain(m *testing.M) {
 	r := run.Run("../config")
 	defer r()
+
+	if err := conf.Read("../config/logic.yml"); err != nil {
+		panic(err)
+	}
+	d = cache.NewRedis(conf.Conf.Redis)
+	if err := d.Ping(); err != nil {
+		os.Exit(-1)
+	}
+	if err := d.Close(); err != nil {
+		os.Exit(-1)
+	}
+	if err := d.Ping(); err == nil {
+		os.Exit(-1)
+	}
+	d = cache.NewRedis(conf.Conf.Redis)
+
 	os.Exit(m.Run())
 }
