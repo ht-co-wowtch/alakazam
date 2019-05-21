a4bf06c8ef2c7347f33773445f7378a5b67cc212
Junx wan <hugh8845sam@gmail.com>
2019-05-21 13:36:45 +0800

    fix: test

--- test/admin/blockade_test.go
+++ test/admin/blockade_test.go
@@ -9,6 +9,8 @@ import (
 
 // 封鎖會員
 func TestSetBlockade(t *testing.T) {
+	request.DialAuth("1000")
+
 	r := request.SetBlockade("82ea16cd2d6a49d887440066ef739669", "測試")
 	assert.Nil(t, r.Error)
 	assert.Equal(t, http.StatusNoContent, r.StatusCode)
@@ -17,6 +19,9 @@ func TestSetBlockade(t *testing.T) {
 
 // 解除某會員封鎖
 func TestDeleteBlockade(t *testing.T) {
+	request.DialAuth("1000")
+	request.SetBlockade("82ea16cd2d6a49d887440066ef739669", "測試")
+
 	r := request.DeleteBlockade("82ea16cd2d6a49d887440066ef739669")
 	assert.Nil(t, r.Error)
 	assert.Equal(t, http.StatusNoContent, r.StatusCode)
