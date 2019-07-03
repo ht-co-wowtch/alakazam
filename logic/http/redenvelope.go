473a81cbe5c53fcc43bd5591f99017ae8bcea15d
Junx wan <hugh8845sam@gmail.com>
2019-07-03 12:18:10 +0800

    feat: 移除沒用method

--- logic/http/redenvelope.go
+++ logic/http/redenvelope.go
@@ -3,9 +3,7 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	"gitlab.com/jetfueltw/cpw/alakazam/client"
-	"gitlab.com/jetfueltw/cpw/alakazam/errors"
 	"gitlab.com/jetfueltw/cpw/alakazam/logic"
-	"gopkg.in/go-playground/validator.v8"
 	"net/http"
 )
 
@@ -115,25 +113,4 @@ func (s *Context) getRedEnvelope(c *gin.Context) error {
 	}
 	c.JSON(http.StatusOK, reply)
 	return nil
-}
-
-func validatorLuckyMoney(c *gin.Context, arg *GiveRedEnvelope) error {
-	var err error
-	if err = c.ShouldBindJSON(arg); err != nil {
-		v := err.(validator.ValidationErrors)
-		for _, e := range v {
-			switch e.Field {
-			case "Amount":
-				err = errors.DataError.Mes("红包金额最低0.01")
-			case "Count":
-				err = errors.DataError.Mes("红包最大数量是500")
-			case "Message":
-				err = errors.DataError.Mes("限制文字长度为1到20个字")
-			default:
-				err = errors.DataError
-			}
-		}
-	}
-
-	return err
 }
