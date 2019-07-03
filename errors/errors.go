bc226e7ff4603e61209786c17c19b7180c4a5c8d
Junx wan <hugh8845sam@gmail.com>
2019-07-03 12:03:22 +0800

    feat: 移除用不到的error邏輯

--- errors/errors.go
+++ errors/errors.go
@@ -2,11 +2,9 @@ package errors
 
 import (
 	"encoding/json"
-	"fmt"
 	"gitlab.com/jetfueltw/cpw/micro/errdefs"
 	"gitlab.com/jetfueltw/cpw/micro/validation"
 	"gopkg.in/go-playground/validator.v8"
-	"net/http"
 )
 
 var (
@@ -18,15 +16,15 @@ var (
 	ErrNoPage = errdefs.NotFound(New("无此Api"))
 	ErrNoRows = errdefs.NotFound(New("没有资料"), 1)
 
-	ConnectError = eNew(http.StatusBadRequest, 10024000, "进入聊天室失败")
-	FailureError = eNew(http.StatusBadRequest, 10024001, "操作失败")
-
-	UserError = eNew(http.StatusBadRequest, 10024003, "取得用户资料失败")
-
-	MoneyError   = eNew(http.StatusUnauthorized, 10024015, "您无法发言，当前发言条件：前%d天充值不少于%d元；打码量不少于%d元")
-	BalanceError = eNew(http.StatusPaymentRequired, 10024020, "您的余额不足发红包")
-	AmountError  = eNew(http.StatusPaymentRequired, 10024021, "金额错误")
-	DataError    = eNew(http.StatusUnprocessableEntity, 10024220, "资料验证错误")
+	//ConnectError = eNew(http.StatusBadRequest, 10024000, "进入聊天室失败")
+	//FailureError = eNew(http.StatusBadRequest, 10024001, "操作失败")
+	//
+	//UserError = eNew(http.StatusBadRequest, 10024003, "取得用户资料失败")
+	//
+	//MoneyError   = eNew(http.StatusUnauthorized, 10024015, "您无法发言，当前发言条件：前%d天充值不少于%d元；打码量不少于%d元")
+	//BalanceError = eNew(http.StatusPaymentRequired, 10024020, "您的余额不足发红包")
+	//AmountError  = eNew(http.StatusPaymentRequired, 10024021, "金额错误")
+	//DataError    = eNew(http.StatusUnprocessableEntity, 10024220, "资料验证错误")
 )
 
 func init() {
@@ -70,46 +68,13 @@ func (m output) Other(err error) string {
 }
 
 type Error struct {
-	Status  int    `json:"-"`
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Message string
 }
 
 func New(message string) Error {
 	return Error{Message: message}
 }
 
-func eNew(status int, code int, message string) Error {
-	return Error{
-		Status:  status,
-		Code:    code,
-		Message: message,
-	}
-}
-
-func eNewB(status int, code int, message string) (Error, []byte) {
-	e := Error{
-		Status:  status,
-		Code:    code,
-		Message: message,
-	}
-	b, err := json.Marshal(e)
-	if err != nil {
-		panic(err)
-	}
-	return e, b
-}
-
 func (e Error) Error() string {
-	return fmt.Sprintf("%d: %v", e.Code, e.Message)
-}
-
-func (e Error) Format(arg ...interface{}) Error {
-	e.Message = fmt.Sprintf(e.Message, arg...)
-	return e
-}
-
-func (e Error) Mes(msg string) Error {
-	e.Message = msg
-	return e
+	return e.Message
 }
