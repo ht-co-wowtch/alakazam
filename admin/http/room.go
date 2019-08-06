8ca0c85490cfe143dae7e088e12a811b9c2c0f1f
Junx wan <hugh8845sam@gmail.com>
2019-08-06 11:58:32 +0800

    feat: 當天數>0 充值與打碼量也可以為0

--- admin/http/room.go
+++ admin/http/room.go
@@ -3,15 +3,13 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"gitlab.com/jetfueltw/cpw/alakazam/errors"
 	"gitlab.com/jetfueltw/cpw/alakazam/logic"
-	"gitlab.com/jetfueltw/cpw/micro/errdefs"
 	"net/http"
 )
 
 func (s *Server) CreateRoom(c *gin.Context) error {
 	var params logic.Room
-	if err := bindRoom(c, &params); err != nil {
+	if err := c.ShouldBindJSON(&params); err != nil {
 		return err
 	}
 	roomId, err := s.logic.CreateRoom(params)
@@ -27,7 +25,7 @@ func (s *Server) CreateRoom(c *gin.Context) error {
 func (s *Server) UpdateRoom(c *gin.Context) error {
 	var params logic.Room
 	params.Id = c.Param("id")
-	if err := bindRoom(c, &params); err != nil {
+	if err := c.ShouldBindJSON(&params); err != nil {
 		return err
 	}
 	if err := s.logic.UpdateRoom(params); err != nil {
@@ -80,17 +78,5 @@ func (s *Server) DeleteRoom(c *gin.Context) error {
 		return err
 	}
 	c.Status(http.StatusNoContent)
-	return nil
-}
-
-func bindRoom(c *gin.Context, params *logic.Room) error {
-	if err := c.ShouldBindJSON(params); err != nil {
-		return err
-	}
-	if params.Limit.Day != 0 {
-		if (params.Limit.Deposit < 0 && params.Limit.Dml < 0) || (params.Limit.Deposit+params.Limit.Dml <= 0) {
-			return errdefs.InvalidParameter(errors.New("需设定打码or充值量"), 2)
-		}
-	}
 	return nil
 }
