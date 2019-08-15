6c7745085e99b586e4ec64333741a1d60bbdcf64
Junx wan <hugh8845sam@gmail.com>
2019-08-15 17:40:46 +0800

    refactor: rename

--- cmd/comet/main.go
+++ cmd/comet/main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"gitlab.com/jetfueltw/cpw/alakazam/comet"
+	"gitlab.com/jetfueltw/cpw/alakazam/comet/api"
 	"gitlab.com/jetfueltw/cpw/alakazam/comet/conf"
-	"gitlab.com/jetfueltw/cpw/alakazam/comet/grpc"
 	"gitlab.com/jetfueltw/cpw/micro/log"
 	"math/rand"
 	"os"
@@ -26,22 +25,22 @@ func main() {
 	if err := conf.Read(confPath); err != nil {
 		panic(err)
 	}
-	fmt.Println("Using config file:", confPath)
+	log.Infof("Using config file: [%s]", confPath)
 
 	rand.Seed(time.Now().UTC().UnixNano())
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// server tcp 連線
 	srv := comet.NewServer(conf.Conf)
+	log.Infof("websocket prot [%s]", conf.Conf.Websocket.Addr)
 	if err := comet.InitWebsocket(srv, conf.Conf.Websocket.Addr, runtime.NumCPU()); err != nil {
 		panic(err)
 	}
 
 	// 啟動grpc server
-	rpcSrv := grpc.New(conf.Conf.RPCServer, srv)
-
-	fmt.Printf("comet start success | websocket: %s | RpcServer: %s\n", conf.Conf.Websocket.Addr, conf.Conf.RPCServer.Addr)
-
+	rpcSrv := api.New(conf.Conf.RPCServer, srv)
+	log.Infof("rpc server port [%s]", conf.Conf.RPCServer.Addr)
+	
 	// 接收到close signal的處理
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
