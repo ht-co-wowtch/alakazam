6dbfe337251771ef9f5174a22b0c02ee89a9543f
Junx wan <hugh8845sam@gmail.com>
2019-08-20 11:31:37 +0800

    feat: mysql 儲存 房間與訊息

--- message/consumer_mysql.go
+++ message/consumer_mysql.go
@@ -19,16 +19,35 @@ func NewMysqlConsumer(db *xorm.EngineGroup) *MysqlConsumer {
 }
 
 const (
-	addMessage = "INSERT INTO `messages` (`seq`,`member_id`,`room_id`,`type`,`message`,`send_at`) VALUES (?,?,?,?,?,?);"
+	addMessage     = "INSERT INTO `messages` (`msg_id`,`member_id`,`message`,`send_at`) VALUES (?,?,?,?);"
+	addRoomMessage = "INSERT INTO `room_messages` (`room_id`,`msg_id`,`type`,`send_at`) VALUES (?,?,?,?);"
 )
 
 func (m *MysqlConsumer) Push(msg *pb.PushMsg) error {
 	sendAt := time.Unix(msg.SendAt, 0)
+	tx := m.db.Master().Prepare()
+
+	defer tx.Rollback()
+	if _, err := tx.Exec(addMessage, msg.Seq, msg.Mid, msg.Message, sendAt); err != nil {
+		log.Error(
+			"insert message",
+			zap.Error(err),
+			zap.Int64("msg_id", msg.Seq),
+			zap.Int64("member_id", msg.Mid),
+			zap.String("message", msg.Message),
+		)
+		return err
+	}
 	for _, rid := range msg.Rids {
-		_, err := m.db.Master().Exec(addMessage, msg.Seq, msg.Mid, rid, msg.Type, msg.Message, sendAt)
-		if err != nil {
-			log.Error("insert message", zap.Error(err), zap.Int64("room", rid), zap.Int64("seq", msg.Seq))
+		if _, err := tx.Exec(addRoomMessage, rid, msg.Seq, msg.Type, sendAt); err != nil {
+			log.Error(
+				"insert room message",
+				zap.Error(err),
+				zap.Int64("room", rid),
+				zap.Int64("msg_id", msg.Seq),
+			)
+			return err
 		}
 	}
-	return nil
+	return tx.Commit()
 }
