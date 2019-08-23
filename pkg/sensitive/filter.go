c052de7590910d71fcc0ff956bf429eab49c108b
Junx wan <hugh8845sam@gmail.com>
2019-08-23 09:45:46 +0800

    feat: unit test and 註解

--- pkg/sensitive/filter.go
+++ pkg/sensitive/filter.go
@@ -3,11 +3,13 @@ package sensitive
 import (
 	"bufio"
 	"io"
+	"sync"
 )
 
 type Filter struct {
 	trie             *trie
 	defaultCharacter rune
+	mu               sync.Mutex
 }
 
 func New() *Filter {
@@ -17,18 +19,25 @@ func New() *Filter {
 	}
 }
 
+// 新增敏感詞
 func (f *Filter) Add(word string) {
+	f.mu.Lock()
 	f.trie.add(word)
+	f.mu.Unlock()
 }
 
 func (f *Filter) Adds(words []string) {
+	f.mu.Lock()
 	for _, word := range words {
 		f.trie.add(word)
 	}
+	f.mu.Unlock()
 }
 
 func (f *Filter) Load(rd io.Reader) error {
 	buf := bufio.NewReader(rd)
+	f.mu.Lock()
+	defer f.mu.Unlock()
 	for {
 		line, _, err := buf.ReadLine()
 		if err != nil {
@@ -42,29 +51,41 @@ func (f *Filter) Load(rd io.Reader) error {
 	return nil
 }
 
+// 過濾敏感詞
 func (f *Filter) Filter(text string) string {
 	return f.trie.Replace(text, f.defaultCharacter)
 }
 
+// 過濾敏感詞
+// 回傳是否有敏感詞
 func (f *Filter) FilterMatch(text string) (string, bool) {
 	str := f.trie.Replace(text, f.defaultCharacter)
 	return str, !(text == str)
 }
 
+// 過濾敏感詞
+// 回傳是否有敏感詞
+// 有哪些敏感詞
 func (f *Filter) FilterFindSensitive(text string) (string, bool, []string) {
 	str, sensitive := f.trie.ReplaceSensitive(text, f.defaultCharacter)
 	return str, !(text == str), sensitive
 }
 
+// 過濾敏感詞，自訂替換詞
 func (f *Filter) Replace(text string, character rune) string {
 	return f.trie.Replace(text, character)
 }
 
+// 過濾敏感詞，自訂替換詞
+// 回傳是否有敏感詞
 func (f *Filter) ReplaceMatch(text string, character rune) (string, bool) {
 	str := f.trie.Replace(text, character)
 	return str, !(text == str)
 }
 
+// 過濾敏感詞，自訂替換詞
+// 回傳是否有敏感詞
+// 有哪些敏感詞
 func (f *Filter) ReplaceFindSensitive(text string, character rune) (string, bool, []string) {
 	str, sensitive := f.trie.ReplaceSensitive(text, character)
 	return str, !(text == str), sensitive
--- pkg/sensitive/filter_test.go
+++ pkg/sensitive/filter_test.go
@@ -1,7 +1,75 @@
 package sensitive
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFilter_Add(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	assert.Equal(t, 1, len(filter.trie.root.children))
+}
+
+func TestFilter_Adds(t *testing.T) {
+	filter := New()
+	filter.Adds([]string{"A", "B"})
+	assert.Equal(t, 2, len(filter.trie.root.children))
+}
 
 func TestFilter(t *testing.T) {
 	filter := New()
+	filter.Add("test")
+	str := filter.Filter("12evtest64")
+	assert.Equal(t, "12ev****64", str)
+}
+
+func TestFilterMatch(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	str, isFilter := filter.FilterMatch("12evtest64")
+
+	assert.True(t, isFilter)
+	assert.Equal(t, "12ev****64", str)
+}
+
+func TestFilterFindSensitive(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	filter.Add("abc")
+	filter.Add("2")
+	str, isFilter, sensitive := filter.FilterFindSensitive("12evtest64")
+
+	assert.True(t, isFilter)
+	assert.Equal(t, "1*ev****64", str)
+	assert.Equal(t, []string{"2", "test"}, sensitive)
+}
+
+func TestFilterReplace(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	str := filter.Replace("12evtest64", '@')
+
+	assert.Equal(t, "12ev@@@@64", str)
+}
+
+func TestFilterReplaceMatch(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	str, isFilter := filter.ReplaceMatch("12evtest64", '@')
+
+	assert.True(t, isFilter)
+	assert.Equal(t, "12ev@@@@64", str)
+}
+
+func TestFilterReplaceFindSensitive(t *testing.T) {
+	filter := New()
+	filter.Add("test")
+	filter.Add("abc")
+	filter.Add("2")
+	str, isFilter, sensitive := filter.ReplaceFindSensitive("12evtest64", '@')
+
+	assert.True(t, isFilter)
+	assert.Equal(t, "1@ev@@@@64", str)
+	assert.Equal(t, []string{"2", "test"}, sensitive)
 }
