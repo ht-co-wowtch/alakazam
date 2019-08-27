14d0a7f95be0097b145abb2e1fe860ff2cd861ba
Junx wan <hugh8845sam@gmail.com>
2019-08-27 16:19:06 +0800

    feat: 增加置頂訊息table來存放

--- message/consumer_mysql.go
+++ message/consumer_mysql.go
@@ -22,12 +22,20 @@ func NewMysqlConsumer(db *xorm.EngineGroup) *MysqlConsumer {
 
 const (
 	addMessage             = "INSERT INTO `messages_%02d` (`msg_id`,`member_id`,`message`,`send_at`) VALUES (?,?,?,?);"
-	addAdminMessage        = "INSERT INTO `admin_messages` (`msg_id`,`room_id`,`message`,`type`,`send_at`) VALUES (?,?,?,?,?);"
+	addAdminMessage        = "INSERT INTO `admin_messages` (`msg_id`,`room_id`,`message`,`send_at`) VALUES (?,?,?,?);"
+	addRoomTopMessage      = "REPLACE INTO `room_top_messages` (`room_id`,`msg_id`,`message`,`send_at`) VALUES (?,?,?,?);"
 	addRedEnvelopeMessages = "INSERT INTO `red_envelope_messages` (`msg_id`,`member_id`,`message`,`red_envelopes_id`,`token`,`expire_at`,`send_at`) VALUES (?,?,?,?,?,?,?);"
 	addRoomMessage         = "INSERT INTO `room_messages_%02d` (`room_id`,`msg_id`,`type`,`send_at`) VALUES (?,?,?,?);"
 )
 
 func (m *MysqlConsumer) Push(msg *pb.PushMsg) error {
+	if msg.Type <= pb.PushMsg_MONEY {
+		return m.Member(msg)
+	}
+	return m.Admin(msg)
+}
+
+func (m *MysqlConsumer) Member(msg *pb.PushMsg) error {
 	sendAt := time.Unix(msg.SendAt, 0)
 	tx := m.db.Master().Prepare()
 
@@ -66,36 +74,55 @@ func (m *MysqlConsumer) Push(msg *pb.PushMsg) error {
 			)
 			return err
 		}
-	case pb.PushMsg_ADMIN, pb.PushMsg_ADMIN_TOP:
-		b, err := json.Marshal(msg.Room)
-		if err != nil {
-			log.Error("json marshal for insert admin message", zap.Int64("msg_id", msg.Seq))
-		}
-		if _, err := tx.Exec(addAdminMessage, msg.Seq, string(b), msg.Message, msg.Type, sendAt); err != nil {
+	}
+
+	for _, rid := range msg.Room {
+		if _, err := tx.Exec(fmt.Sprintf(addRoomMessage, rid%50), rid, msg.Seq, msg.Type, sendAt); err != nil {
 			log.Error(
-				"insert admin message",
+				"insert room message",
 				zap.Error(err),
+				zap.Int32("room", rid),
 				zap.Int64("msg_id", msg.Seq),
-				zap.Int64("member_id", msg.Mid),
-				zap.String("message", msg.Message),
 			)
 			return err
 		}
 	}
+	return tx.Commit()
+}
 
-	if msg.Type <= pb.PushMsg_MONEY {
+func (m *MysqlConsumer) Admin(msg *pb.PushMsg) error {
+	sendAt := time.Unix(msg.SendAt, 0)
+	tx := m.db.Master().Prepare()
+
+	defer tx.Rollback()
+
+	switch msg.Type {
+	case pb.PushMsg_ADMIN:
+		b, err := json.Marshal(msg.Room)
+		if err != nil {
+			log.Error("json marshal for insert admin message", zap.Int64("msg_id", msg.Seq))
+		}
+		if _, err := tx.Exec(addAdminMessage, msg.Seq, string(b), msg.Message, sendAt); err != nil {
+			log.Error(
+				"insert admin message",
+				zap.Error(err),
+				zap.Int64("msg_id", msg.Seq),
+				zap.String("message", msg.Message),
+			)
+			return err
+		}
+	case pb.PushMsg_ADMIN_TOP:
 		for _, rid := range msg.Room {
-			if _, err := tx.Exec(fmt.Sprintf(addRoomMessage, rid%50), rid, msg.Seq, msg.Type, sendAt); err != nil {
+			if _, err := tx.Exec(addRoomTopMessage, rid, msg.Seq, msg.Message, sendAt); err != nil {
 				log.Error(
-					"insert room message",
+					"insert room top message",
 					zap.Error(err),
-					zap.Int32("room", rid),
 					zap.Int64("msg_id", msg.Seq),
+					zap.String("message", msg.Message),
 				)
 				return err
 			}
 		}
-		return tx.Commit()
 	}
-	return nil
+	return tx.Commit()
 }
