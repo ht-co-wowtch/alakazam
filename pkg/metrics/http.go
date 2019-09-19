fb3dfee6f312185a516a204ba325a50ba7dce4de
王俊雄 <hugh8845sam@gmail.com>
2019-09-19 14:29:56 +0800

    feat: 增加healthz給k8做健康檢查

--- pkg/metrics/http.go
+++ pkg/metrics/http.go
@@ -3,15 +3,20 @@ package metrics
 import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"gitlab.com/jetfueltw/cpw/micro/log"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func RunHttp(addr string) {
 	go func() {
+		log.Infof("metrics server port [:%s]", addr)
 		http.Handle("/metrics", promhttp.Handler())
+		http.HandleFunc("/healthz", func(rsp http.ResponseWriter, req *http.Request) {
+			rsp.WriteHeader(http.StatusOK)
+		})
 		err := http.ListenAndServe(addr, nil)
 		if err != nil && err != http.ErrServerClosed {
-			log.Error(err.Error())
+			log.Errorf("metrics server", zap.Error(err))
 		}
 	}()
 }
