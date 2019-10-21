7a1360fd404f7725119639af5d2a22de167d9e00
Junx wan <hugh8845sam@gmail.com>
2019-10-21 12:16:31 +0800

    發跟投訊息增加items欄位(組合號碼)

--- message/json.go
+++ message/json.go
@@ -57,8 +57,9 @@ type Bets struct {
 }
 
 type Bet struct {
-	Name     string  `json:"name" binding:"required"`
-	Odds     float64 `json:"odds" binding:"required"`
-	OddsCode string  `json:"odds_code" binding:"required"`
-	Amount   int     `json:"amount" binding:"required"`
+	Name     string   `json:"name" binding:"required"`
+	Odds     float64  `json:"odds" binding:"required"`
+	OddsCode string   `json:"odds_code" binding:"required"`
+	Items    []string `json:"items"`
+	Amount   int      `json:"amount" binding:"required"`
 }
