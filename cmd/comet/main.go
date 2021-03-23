edadf0c2c1a9f14e327b4c93ab4deafd0b28853f
zorn <zorntsai@jetfuel.com.tw>
2021-03-23 18:33:28 +0800

    remove comet log

--- cmd/comet/main.go
+++ cmd/comet/main.go
@@ -39,14 +39,6 @@ func main() {
 	srv := comet.NewServer(conf.Conf)
 	log.Infof("websocket prot [%s]", conf.Conf.Websocket.Addr)
 
-	//ZDbg
-	log.Infof("conf.DB %v", conf.Conf.DB)
-
-	/*ZDbg
-	if err := comet.InitWebsocket(srv, conf.Conf.Websocket.Addr, runtime.NumCPU()); err != nil {
-		panic(err)
-	}*/
-
 	if err := comet.InitWebsocket(srv, conf.Conf.Websocket.Addr, runtime.NumCPU(), conf.Conf.DB); err != nil {
 		panic(err)
 	}
